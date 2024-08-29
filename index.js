8f5ab885cce47e7554b7593ecae399326088392d
t9os <165030765+T9lAT22@users.noreply.github.com>
2024-08-29 09:56:18 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+  document.addEventListener('keydown', function() {
+            // Replace 'https://www.example.com' with the URL you want to navigate to
+            window.location.href = 't9os.html';
+        });
